d50b253d882b80840517890c9d5c5f291ace14c0
rohitepur <rohitepur@gmail.com>
2020-04-20 12:52:58 -0400

    Module 2 - 1

---
+++ src/app/product.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Response } from '@angular/http';
+
+@Injectable()
+export class ProductService {
+
+  constructor () { 
+
+  }
+
+}
